f387800673e320dc4a93edf6e75bce4307ff2b5e
Slawek <roshendal@gmail.com>
2017-10-22 17:56:51 +0200

    transfer model spec

--- spec/models/transfer_spec.rb
+++ spec/models/transfer_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Transfer, type: :model do
-  it { should belong_to(:user) }
+  describe "Associations" do
+    it { should belong_to(:user) }
+  end
   
-  # Validation test
-  it { should validate_presence_of(:account_number_from) }
-  it { should validate_presence_of(:account_number_to) }
-  it { should validate_presence_of(:country_code_from) }
-  it { should validate_presence_of(:country_code_to) }
+  describe "Validations" do
+    it { should validate_presence_of(:account_number_from) }
+    it { should validate_presence_of(:account_number_to) }
+    it { should validate_presence_of(:country_code_from) }
+    it { should validate_presence_of(:country_code_to) }
 
-  it { should validate_length_of(:account_number_from).is_equal_to(18) }
-  it { should validate_length_of(:account_number_to).is_equal_to(18) }
-  it { should validate_length_of(:country_code_from).is_equal_to(3) }
-  it { should validate_length_of(:country_code_to).is_equal_to(3) }
+    it { should validate_length_of(:account_number_from).is_equal_to(18) }
+    it { should validate_length_of(:account_number_to).is_equal_to(18) }
+    it { should validate_length_of(:country_code_from).is_equal_to(3) }
+    it { should validate_length_of(:country_code_to).is_equal_to(3) }
+  end
 end
